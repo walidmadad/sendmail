77f02a5e3d824ce1b6c961b84cf5c0653ca7ba9c
walidmadad <walid.madad123@gmail.com>
2025-02-03 19:03:09 +0100

    add UserRepository

---
+++ src/main/java/com/example/mailsend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.mailsend.repository;
+
+import com.example.mailsend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationToken(String token);
+}
+
